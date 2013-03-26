b869156fc8732af50b71b8dc8e2741fd2077ebc7
Gabriel <belmonte.gabriel.k@gmail.com>
2013-03-25 23:22:44 -0400

    Added initial thingy for accession number

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,7 +1,14 @@
+function FetchAccessionNumberSequence()
+{
+	
+	$.post("http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id="+$("#accession").find("textarea").val()+"&rettype=fasta&retmode=text");
+}
+
 window.onload = function() {
     var button1 = document.getElementById("submit1");
     button1.addEventListener("click",nextStep,false);
-};
+	$('#submit_ACN').click(FetchAccessionNumberSequence);
+	};
 
 function nextStep() {
     var div_step1 = document.getElementById("step1");
@@ -9,3 +16,5 @@ function nextStep() {
     div_step1.className+= " hidden";
     div_step2.className= "step";
 }   
+
+
