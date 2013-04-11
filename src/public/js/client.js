39e0c563dfc329a11fb2be6e3d2e91ba82837134
Anas Ambri <anas.ambri@gmail.com>
2013-04-10 21:19:43 -0400

    Changed fileLoader from object to module pattern, refactored some methods, removed button to move to step 2

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,83 +1,67 @@
-var data;
-
-//Sequence selection mechanisms
-
 //Fetch from database
 function FetchAccessionNumberSequence()
 {
-    var sequence = $("#accession").find("textarea").val();
+    var sequence = $("#accession").find("input").val();
     //var url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id="+
-            sequence+"&rettype=fasta&retmode=text";
-    var url ="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi";
+    //sequence + "&rettype=fasta&retmode=text";
+    console.log("sequence accession # " + sequence);
+    var url = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi";
     $.ajax({
-          type: "GET",
-          url : url,
-          data : {
-              'db': 'nuccore',
-              'id' : sequence,
-              'rettype' : 'fasta',
-              'retmode' : 'text'
-           },
-          success : function(d){
-                data = d;
-                $("#sequenceDisplay")[0].value = d.toString();
-          },
-          error : function(jqXHR, textStatus, errorThrown ){
-            switch(textStatus){
+        type: "GET",
+        url: url,
+        data: {
+            db: 'nuccore',
+            'id': sequence,
+            rettype: 'fasta',
+            retmode: 'text'
+        },
+        success: function(d) {
+            loadInputToDisplay(d.toString());
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            switch (textStatus) {
                 case "error":
-                    alert("errorThrown is "+errorThrown);
+                    alert("errorThrown is " + errorThrown);
             }
-          }
+        }
     });
 }
 
+function loadInputToDisplay(str){
+    //TODO Add stuff to clean out input text
+    $("#sequence-display")[0].value = str;
+}
+
 //File drop
-var fileLoader = {
-    reader : new FileReader(),
-    file : {
-        name : ""
-    },
-    ReadFile : function(fileToRead){
-        this.reader.readAsText(fileToRead);
-        $('.head').text(this.file.name.substr(0, this.file.name.length - 4));
-        this.reader.onload = this.LoadFile;
-    },
-    LoadFile: function (){
-        var value = fileLoader.reader.result;
-        $("#sequenceDisplay")[0].value = value;
-    },
-    handleFileSelect: function(evt){
-        evt.stopPropagation();
-        evt.preventDefault();
-
-        files = evt.dataTransfer.files; // FileList object.
-
-        // files is a FileList of File objects. List some properties.
-        var file = files[0];
-        fileLoader.ReadFile(file);
-    },
-    handleDragOver : function(evt){
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-    }
+var fileLoader = function() {
+    var reader = new FileReader();
+    var file;
+    return {
+        readFile: function(fileToRead) {
+            reader.readAsText(fileToRead);
+            $('.head').text(file.name.substr(0, file.name.length - 4));
+            reader.onload = function() {
+                loadInputToDisplay(reader.result);
+            };
+        },
+        handleFileSelect: function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            file = evt.dataTransfer.files[0]; // file object uploaded
+            fileLoader.ReadFile(file);
+        },
+        handleDragOver: function(evt) {
+            evt.stopPropagation();
+            evt.preventDefault();
+            evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+        }
+    };
 };
-
 
 window.onload = function() {
     var button1 = document.getElementById("submit1");
-    button1.addEventListener("click", nextStep, false);
     $('#submit_ACN').click(FetchAccessionNumberSequence);
     var dropZone = document.getElementById('drop-zone');
     dropZone.addEventListener('dragover', fileLoader.handleDragOver, false);
     dropZone.addEventListener('drop', fileLoader.handleFileSelect, false);
 };
-
-function nextStep() {
-    var div_step1 = document.getElementById("step1");
-    var div_step2 = document.getElementById("step2");
-    div_step1.className += " hidden";
-    div_step2.className = "step";
-}
-
-
