d64297020446bc29de5edd0bd8761ea61f98c158
Gabriel <belmonte.gabriel.k@gmail.com>
2013-08-04 18:32:29 -0400

    removed old folder

--- src/algorithms_deprecated/DebugLog.js
+++
@@ -1,9 +0,0 @@
-var LOG_LEVEL = 0;// 0: Only super important stuff
-					// 30: Everything
-function Log(message, module, level)
-{
-	if(level < LOG_LEVEL)
-		console.log(module+": "+ message);
-}
-
-
